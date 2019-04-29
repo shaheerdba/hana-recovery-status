c8e0c3eeaa7c2aa2245c825ab8e1b7f8c62148c3
shaheerdba <49828191+shaheerdba@users.noreply.github.com>
2019-04-29 16:42:47 +0200

    precise information on recovery completion status
     BACKUP LOG INFO (LAST FEW LINES)
    
    2019-04-29T12:22:01+02:00  P039570      16a65edcba3 INFO    RECOVERY RECOVER DATA finished successfully, reached timestamp 2019-04-26T09:39:02+02:00, reached log position 227547169282

--- rstat.sh
+++ rstat.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/sh
 ### IMPORTANT NOTE: If any modification please contact i348761
 . $HOME/.sapenv.sh
-gp=`sapcontrol -nr 06 -function GetProcessList`
+#gp=`sapcontrol -nr 06 -function GetProcessList`
+val1=`tail $DIR_INSTANCE/$VTHOSTNAME/trace/backup.log | grep successfully | awk '{print $3}'`
+val2=`egrep 'RECOVERY RECOVER DATA started' $DIR_INSTANCE/$VTHOSTNAME/trace/backup.log -A1 | tail -1 | awk '{print $3}'`
+val3=`tail $DIR_INSTANCE/$VTHOSTNAME/trace/backup.log | grep successfully | awk '{print $8}'`
 echo -e "\nsystem timestamp: `date`\n"
 echo -e "memory status:\n `free -g -t`"
 free_mem=`free -g -t | grep Mem | awk '{print $4}'`
@@ -15,5 +18,11 @@ echo -e "\nRECOVERY DETAILS:\n"
 egrep 'RECOVERY RECOVER DATA started' $DIR_INSTANCE/$VTHOSTNAME/trace/backup.log -A1 | tail -2
 echo -e "\n"
 for i in `ps -eaf | grep hdbindexserver | grep -v grep | awk '{print $10}'| grep .`;do echo -e "\nstatus of indexserver $i\n" ; grep "RECOVERY progress of service: indexserver, sc12hdbr1601:$i*, volume: [0-9]" $DIR_INSTANCE/$VTHOSTNAME/trace/backup.log | tail;done
-echo -e "\n BACKUP LOG INFO (LAST 10 LINES)\n"
-tail -f $DIR_INSTANCE/$VTHOSTNAME/trace/backup.log
+echo -e "\n BACKUP LOG INFO (LAST FEW LINES)\n"
+
+if [[ "$val1" = "$val2" && "$val3" = "finished" ]]; then
+        tail $DIR_INSTANCE/$VTHOSTNAME/trace/backup.log | grep successfully
+else
+        tail -f $DIR_INSTANCE/$VTHOSTNAME/trace/backup.log
+fi
+exit 0
